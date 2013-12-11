26cddf8b45ea0187bd4fdd8dfbd695dca5fa3a04
scottlibert@gmail.com <scottlibert@gmail.com@ad49494d-ed8a-5977-8b63-c7e2cf452bcb>
2013-12-11 20:24:55 +0000

    Fixed up the headers in the file to reflect correct licensing information and attribution.

--- trunk/crio/IMU.cpp
+++ trunk/crio/IMU.cpp
@@ -1,3 +1,10 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) Kauai Labs. All Rights Reserved.							  */
+/* Based upon the Open Source WPI Library released by FIRST robotics.         */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in $(WIND_BASE)/WPILib.  */
+/*----------------------------------------------------------------------------*/
+
 
 #include "NetworkCommunication/UsageReporting.h"
 #include "Timer.h"
