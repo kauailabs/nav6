5187bde11963a5981d8bf125d5d56cf57bdc57ae
scottlibert@gmail.com <scottlibert@gmail.com@ad49494d-ed8a-5977-8b63-c7e2cf452bcb>
2013-12-28 20:23:50 +0000

    renamed from IMU_Protocol

---
+++ arduino/nav6ProtocolTest/nav6ProtocolTest.ino
@@ -0,0 +1,38 @@
+#include "IMUProtocol.h"
+
+void setup()
+{
+ Serial.begin(57600);
+ Serial.println("Init"); 
+}
+
+char protocol_buff[256];
+
+float yaw = 1.06;
+float pitch = 162.1;
+float roll = -13.28;
+float compass_heading = 17.5;
+
+void loop()
+{
+  int num_bytes = IMUProtocol::encodeYPRUpdate( protocol_buff, yaw, pitch, roll, compass_heading );
+  Serial.write((byte *)protocol_buff,num_bytes);
+  delay(1000);
+  float decodedyaw, decodedpitch, decodedroll, decodedcompassheading;
+  if ( IMUProtocol::decodeYPRUpdate( protocol_buff, num_bytes, decodedyaw, decodedpitch, decodedroll, decodedcompassheading ) )
+  {
+    Serial.print("Decoded.  Yaw:  "  );
+    Serial.print(decodedyaw);
+    Serial.print(" Pitch:  ");
+    Serial.print(decodedpitch);
+    Serial.print(" Roll:  " );
+    Serial.println(decodedroll);
+    Serial.print(" Heading:  " );
+    Serial.println(decodedcompassheading);
+  }
+  else
+  {
+    Serial.println("Error Decoding");
+  }
+}
+
