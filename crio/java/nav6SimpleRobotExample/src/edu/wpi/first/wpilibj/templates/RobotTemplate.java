b39c03c2471d307b45a37d67c815a12f83445a37
scottlibert@gmail.com <scottlibert@gmail.com@ad49494d-ed8a-5977-8b63-c7e2cf452bcb>
2014-01-18 16:50:59 +0000

    Added while loop to code that updates dashboard in teleop, based on issue found/reported by Will Martin (Team 11).

--- crio/java/nav6SimpleRobotExample/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ crio/java/nav6SimpleRobotExample/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -12,6 +12,7 @@ import com.kauailabs.nav6.frc.IMU;
 //import com.kauailabs.nav6.frc.IMUAdvanced; Comment this in to use "Advanced" features
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.visa.VisaException;
@@ -34,11 +35,13 @@ public class RobotTemplate extends SimpleRobot {
             
             // You can add a second parameter to modify the 
             // update rate (in hz) from 4 to 100.  The default is 100.
+            // To help reduce CPU load, set it to 50.
             
             // You can also use the IMUAdvanced class for advanced
             // features.
             
-            imu = new IMU(serial_port);
+            imu = new IMU(serial_port,(byte)50);
+            //imu = new IMUAdvanced(serial_port,(byte)50);
         } catch (VisaException ex) {
             ex.printStackTrace();
         }
@@ -57,23 +60,24 @@ public class RobotTemplate extends SimpleRobot {
      */
     public void operatorControl() {
 
-        SmartDashboard.putBoolean("IMU_Connected", imu.isConnected());
-        SmartDashboard.putNumber("IMU_Yaw", imu.getYaw());
-        SmartDashboard.putNumber("IMU_Pitch", imu.getPitch());
-        SmartDashboard.putNumber("IMU_Roll", imu.getRoll());
-        SmartDashboard.putNumber("IMU_CompassHeading", imu.getCompassHeading());
-	SmartDashboard.putNumber("IMU_Update_Count", imu.getUpdateCount());
-	SmartDashboard.putNumber("IMU_Byte_Count", imu.getByteCount());
-        
-        // If you are using the IMUAdvanced class, you can also access the following
-        // additional functions, at the expense of some extra processing
-        // that occurs on the CRio processor
-        
-        // SmartDashboard.putNumber("IMU_Accel_X", imu.getWorldLinearAccelX() );
-        // SmartDashboard.putNumber("IMU_Accel_Y", imu.getWorldLinearAccelY() );
-        // SmartDashboard.putBoolean("IMU_IsMoving", imu.isMoving() );
-        // SmartDashboard.putNumber("IMU_Temp_C", imu.getTempC() );
-    }
+        while (isOperatorControl() && isEnabled()) {
+            SmartDashboard.putBoolean("IMU_Connected", imu.isConnected());
+            SmartDashboard.putNumber("IMU_Yaw", imu.getYaw());
+            SmartDashboard.putNumber("IMU_Pitch", imu.getPitch());
+            SmartDashboard.putNumber("IMU_Roll", imu.getRoll());
+            SmartDashboard.putNumber("IMU_CompassHeading", imu.getCompassHeading());
+            SmartDashboard.putNumber("IMU_Update_Count", imu.getUpdateCount());
+            SmartDashboard.putNumber("IMU_Byte_Count", imu.getByteCount());
+            // If you are using the IMUAdvanced class, you can also access the following
+            // additional functions, at the expense of some extra processing
+            // that occurs on the CRio processor
+//            SmartDashboard.putNumber("IMU_Accel_X", imu.getWorldLinearAccelX());
+//            SmartDashboard.putNumber("IMU_Accel_Y", imu.getWorldLinearAccelY());
+//            SmartDashboard.putBoolean("IMU_IsMoving", imu.isMoving());
+//            SmartDashboard.putNumber("IMU_Temp_C", imu.getTempC());
+            Timer.delay(0.01);
+        }
+     }
     
     /**
      * This function is called once each time the robot enters test mode.
