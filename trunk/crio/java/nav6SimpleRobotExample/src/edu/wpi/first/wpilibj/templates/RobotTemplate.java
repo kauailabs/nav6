a7d2a39447b6d2a60b16965199c03fe3447a2cbc
scottlibert@gmail.com <scottlibert@gmail.com@ad49494d-ed8a-5977-8b63-c7e2cf452bcb>
2014-01-29 18:40:37 +0000

    Modified to use isCalibration() for IMU class, now that this method is in the base class

--- trunk/crio/java/nav6SimpleRobotExample/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ trunk/crio/java/nav6SimpleRobotExample/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -27,8 +27,8 @@ import edu.wpi.first.wpilibj.visa.VisaException;
 public class RobotTemplate extends SimpleRobot {
     
     BufferingSerialPort serial_port;
-    //IMU imu;  // Alternatively, use IMUAdvanced for advanced features
-    IMUAdvanced imu;
+    IMU imu;  // Alternatively, use IMUAdvanced for advanced features
+    //IMUAdvanced imu;
     boolean first_iteration;
     
     public RobotTemplate() {
@@ -43,9 +43,9 @@ public class RobotTemplate extends SimpleRobot {
             // You can also use the IMUAdvanced class for advanced
             // features.
 
-            byte update_rate_hz = 20;
-            //imu = new IMU(serial_port,update_rate_hz);
-            imu = new IMUAdvanced(serial_port,update_rate_hz);
+            byte update_rate_hz = 50;
+            imu = new IMU(serial_port,update_rate_hz);
+            //imu = new IMUAdvanced(serial_port,update_rate_hz);
         } catch (VisaException ex) {
             ex.printStackTrace();
         }
@@ -68,27 +68,40 @@ public class RobotTemplate extends SimpleRobot {
     public void operatorControl() {
 
         while (isOperatorControl() && isEnabled()) {
+            
+            // When calibration has completed, zero the yaw
+            // Calibration is complete approaximately 20 seconds
+            // after the robot is powered on.  During calibration,
+            // the robot should be still
+            
             boolean is_calibrating = imu.isCalibrating();
             if ( first_iteration && !is_calibrating ) {
                 Timer.delay( 0.3 );
                 imu.zeroYaw();
                 first_iteration = false;
             }
-            SmartDashboard.putBoolean("IMU_Connected", imu.isConnected());
-            SmartDashboard.putNumber("IMU_Yaw", imu.getYaw());
-            SmartDashboard.putNumber("IMU_Pitch", imu.getPitch());
-            SmartDashboard.putNumber("IMU_Roll", imu.getRoll());
-            SmartDashboard.putNumber("IMU_CompassHeading", imu.getCompassHeading());
-            SmartDashboard.putNumber("IMU_Update_Count", imu.getUpdateCount());
-            SmartDashboard.putNumber("IMU_Byte_Count", imu.getByteCount());
+            
+            // Update the dashboard with status and orientation
+            // data from the nav6 IMU
+            
+            SmartDashboard.putBoolean(  "IMU_Connected",        imu.isConnected());
+            SmartDashboard.putBoolean(  "IMU_IsCalibrating",    imu.isCalibrating());
+            SmartDashboard.putNumber(   "IMU_Yaw",              imu.getYaw());
+            SmartDashboard.putNumber(   "IMU_Pitch",            imu.getPitch());
+            SmartDashboard.putNumber(   "IMU_Roll",             imu.getRoll());
+            SmartDashboard.putNumber(   "IMU_CompassHeading",   imu.getCompassHeading());
+            SmartDashboard.putNumber(   "IMU_Update_Count",     imu.getUpdateCount());
+            SmartDashboard.putNumber(   "IMU_Byte_Count",       imu.getByteCount());
+
             // If you are using the IMUAdvanced class, you can also access the following
             // additional functions, at the expense of some extra processing
             // that occurs on the CRio processor
-            SmartDashboard.putNumber("IMU_Accel_X", imu.getWorldLinearAccelX());
-            SmartDashboard.putNumber("IMU_Accel_Y", imu.getWorldLinearAccelY());
-            SmartDashboard.putBoolean("IMU_IsMoving", imu.isMoving());
-            SmartDashboard.putNumber("IMU_Temp_C", imu.getTempC());
-            SmartDashboard.putBoolean("IMU_IsCalibrating", imu.isCalibrating());
+            
+            //SmartDashboard.putNumber(   "IMU_Accel_X",          imu.getWorldLinearAccelX());
+            //SmartDashboard.putNumber(   "IMU_Accel_Y",          imu.getWorldLinearAccelY());
+            //SmartDashboard.putBoolean(  "IMU_IsMoving",         imu.isMoving());
+            //SmartDashboard.putNumber(   "IMU_Temp_C",           imu.getTempC());
+            
             Timer.delay(0.2);
         }
      }
